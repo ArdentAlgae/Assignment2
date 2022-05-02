248784d0e654e1cf2886ac54ad4f4138ed635af0
aiden <aidencgbarlow@gmail.com>
2022-05-02 16:31:23 +1000

    Class views nearly done, just need some polish. Added in the DatabaseController class for the moment so I could put all the methods in, but it'll  need to be changed.

---
+++ Assignment2/Assignment2/ClassView.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment2
+{
+    /// <summary>
+    /// Interaction logic for AddClassView.xaml
+    /// </summary>
+    public partial class ClassView : UserControl
+    {
+        Class class_;
+        public ClassView(Class class_)
+        {
+            this.class_ = class_;
+            InitializeComponent();
+
+            unitCodeText.Text = class_.unitCode;
+            campusText.Text = class_.campus;
+            dayText.Text = class_.day;
+            startTimeText.Text = class_.startTime.ToString();
+            endTimeText.Text = class_.endTime.ToString();
+            typeText.Text = class_.type;
+            roomText.Text = class_.room;
+            staffText.Text = class_.staff.ToString();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            HomeView home = new HomeView();
+            Body.setBody(home);
+        }
+
+        private void editButtonClicked(object sender, RoutedEventArgs e)
+        {
+            EditClassView view = new EditClassView(class_);
+            Body.setBody(view);
+        }
+
+        private void deleteButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete this class? This cannot be undone.", "Confirmation", MessageBoxButton.OKCancel) == MessageBoxResult.Yes)
+            {
+                DatabaseController.deleteClass(class_);
+                HomeView home = new HomeView();
+                Body.setBody(home);
+            }
+        }
+    }
+}
---
+++ Assignment2/Assignment2/EditClassView.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment2
+{
+    /// <summary>
+    /// Interaction logic for EditClassView.xaml
+    /// </summary>
+    public partial class EditClassView : UserControl
+    {
+        Class class_;
+        public EditClassView(Class class_)
+        {
+            this.class_ = class_;
+            InitializeComponent();
+
+            unitCodeTextBox.Text = class_.unitCode;
+            campusTextBox.Text = class_.campus;
+            dayTextBox.Text = class_.day;
+            startTimeTextBox.Text = class_.startTime.ToString();
+            endTimeTextBox.Text = class_.endTime.ToString();
+            typeTextBox.Text = class_.type;
+            roomTextBox.Text = class_.room;
+            staffTextBox.Text = class_.staff.ToString();
+        }
+
+        private void ConfirmButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Save class?", "Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                Class newClass = new Class();
+                newClass.unitCode = unitCodeTextBox.Text;
+                newClass.campus = campusTextBox.Text;
+                newClass.day = dayTextBox.Text;
+                newClass.startTime = Int32.Parse(startTimeTextBox.Text);
+                newClass.endTime = Int32.Parse(endTimeTextBox.Text);
+                newClass.type = typeTextBox.Text;
+                newClass.room = roomTextBox.Text;
+                newClass.staff = Int32.Parse(staffTextBox.Text);
+                DatabaseController.replaceClass(class_, newClass);
+
+                ClassView view = new ClassView(newClass);
+                Body.setBody(view);
+            }
+        }
+
+        private void DiscardButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Discard changes?", "Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                ClassView view = new ClassView(class_);
+                Body.setBody(view);
+            }
+        }
+    }
+}
