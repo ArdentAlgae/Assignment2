2831599ac4ce2e83bad78e2e2d86adcc7ba15e6d
Tom Storey <mail@tmstorey.id.au>
2022-05-03 14:38:21 +1000

    Allow editing of DataGrid rows as objects

--- Assignment2/Assignment2/Staff.cs
+++ Assignment2/Assignment2/Staff.cs
@@ -13,12 +13,23 @@ namespace Assignment2
         public string familyName { get; set; }
         public string title { get; set; }
         public string campus { get; set; }
-        public int phone { get; set; }
+        public string phone { get; set; }
         public string room { get; set; }
         public string email { get; set; }
         public Staff()
         {
 
+        }
+        public Staff(object[] rowData)
+        {
+            staffID = (int) rowData[0];
+            givenName = (string) rowData[1];
+            familyName = (string)rowData[2];
+            title = (string) rowData[3];
+            campus = (string) rowData[4];
+            phone = (string) rowData[5];
+            room = (string) rowData[6];
+            email = (string) rowData[7];
         }
     }
 }
