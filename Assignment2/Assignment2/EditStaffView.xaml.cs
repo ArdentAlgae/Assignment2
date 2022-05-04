aa1303c6c652d538003fff3c167e2660ff2d2b5a
Tom Storey <thomasstorey7@gmail.com>
2022-05-05 00:04:52 +1000

    Fix Edit Staff view

--- Assignment2/Assignment2/EditStaffView.xaml.cs
+++ Assignment2/Assignment2/EditStaffView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Assignment2
 {
@@ -20,9 +21,12 @@ namespace Assignment2
     /// </summary>
     public partial class EditStaffView : UserControl
     {
+        public Staff staff_;
         public EditStaffView(Staff staff)
         {
             InitializeComponent();
+
+            staff_ = staff;
 
             staffIDTextBox.Text = staff.staffID.ToString();
             givenNameTextBox.Text = staff.givenName;
@@ -41,6 +45,43 @@ namespace Assignment2
             {
                 Staff newStaff = new Staff();
                 newStaff.staffID = Int32.Parse(staffIDTextBox.Text);
+                newStaff.campus = campusTextBox.Text;
+                newStaff.givenName = givenNameTextBox.Text;
+                newStaff.familyName = familyNameTextBox.Text;
+                newStaff.title = titleTextBox.Text;
+                newStaff.phone = phoneTextBox.Text;
+                newStaff.room = roomTextBox.Text;
+                newStaff.email = emailTextBox.Text;
+                newStaff.profile = new byte[0];
+                using (var conn = new MySqlConnection("Database=hris;Data Source=alacritas.cis.utas.edu.au;User Id=kit206g2a;Password=group2a"))
+                {
+                    conn.Open();
+                    MySqlCommand cmd;
+                    cmd = new MySqlCommand("UPDATE staff" +
+                        " SET id=@NewID," +
+                        " campus=@NewCampus," +
+                        " given_name=@NewGivenName," +
+                        " family_name=@NewFamilyName," +
+                        " title=@NewTitle," +
+                        " phone=@NewPhone," +
+                        " room=@NewRoom," +
+                        " email=@NewEmail" +
+                        " WHERE id=@staffID", conn);
+
+                    cmd.Parameters.AddWithValue("@staffID", staff_.staffID);
+
+                    cmd.Parameters.AddWithValue("@NewID", newStaff.staffID);
+                    cmd.Parameters.AddWithValue("@NewCampus", newStaff.campus);
+                    cmd.Parameters.AddWithValue("@NewGivenName", newStaff.givenName);
+                    cmd.Parameters.AddWithValue("@NewFamilyName", newStaff.familyName);
+                    cmd.Parameters.AddWithValue("@NewTitle", newStaff.title);
+                    cmd.Parameters.AddWithValue("@NewPhone", newStaff.phone);
+                    cmd.Parameters.AddWithValue("@NewRoom", newStaff.room);
+                    cmd.Parameters.AddWithValue("@NewEmail", newStaff.email);
+
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
 
                 StaffView view = new StaffView(newStaff);
                 Body.setBody(view);
